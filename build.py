9f6e3b447d9e8238b89b8adbbd7083391c62d380
gsbirch <gsbirchtree@gmail.com>
2026-09-02 11:43:13 -0400

    update build.py

--- build.py
+++ build.py
@@ -9,16 +9,29 @@ CSV_DIR = "csv"
 PLANS_DIR = "plans"
 # The directory where domain txt files can be found
 DOMAINS_DIR = "domains"
+# The directory where tool output will be placed
+OUTPUT_DIR = "out"
+# The node limit for each search
+NODE_LIMIT = 10000
+# The path of the jar of the verification tool
+JAR = "verify.jar"
 
-folder = Path(CSV_DIR)
+# The messages that print out when the tool succeeds/fails in finding an explanation
+SUCCESS_MESSAGE = "This plan can be explained by Sabre within the given constraints"
+FAIL_MESSAGE = "This plan can not be explained by Sabre within the given constraints"
 
-fileNames = [file.name for file in folder.glob("*.csv")]
+csv_folder = Path(CSV_DIR)
 
+fileNames = [file.name for file in csv_folder.glob("*.csv")]
+
+# dictionaries for the different types of searches performed
+# right now this is just astar search w/ temporal cost
 search_to_m = { "astar": "bf" }
 cost_to_c = { "temporal": "t" }
 
 with open(OUTPUT_PROGRAM, "w") as out:
     out.write("#!/bin/bash\n")
+    out.write("failures=0\n")
 
     # generate tests for each csv file
     for fileName in fileNames:
@@ -28,32 +41,41 @@ with open(OUTPUT_PROGRAM, "w") as out:
             # generate one test for each row
             for row in data:
                 # the file where the plan will be generated
-                plan_file = f"{PLANS_DIR}/{row['problem'][:-4]}_{int(float(row['goal']))}_{row['solution_index']}.txt"
+                test_name = f"{row['problem'][:-4]}_{int(float(row['goal']))}_{row['solution_index']}"
+                plan_file = f"{PLANS_DIR}/{test_name}_PLAN.txt"
                 # write the plan to a txt file
                 with open(plan_file, "w") as plan:
                     str = row['author_signature'].replace(") ", ")\n")
                     plan.write(str)
 
                 # the file where the solution will be generated
-                solution_file = f"{PLANS_DIR}/{row['problem'][:-4]}_{int(float(row['goal']))}_{row['solution_index']}_SOLUTION.txt"
+                solution_file = f"{PLANS_DIR}/{test_name}_SOLUTION.txt"
                 # write the solution to a txt file
                 with open(solution_file, "w") as plan:
                     str = row['plan'].replace(") |", ")\n")
                     plan.write(str)
 
-                # the command to be ran
-                output = f"java -jar sabre.jar -p {DOMAINS_DIR}/{row['problem']} -pl {plan_file} -g {row['goal']} -vl {10000} -atl {row['atl']} "
-                output += f"-ctl {row['ctl'] + 1} -el {row['el'] + 1} -m {search_to_m[row['search']]} -c {cost_to_c[row['cost']]} -h {row['heuristic']}"
-                print(output)
+                out_file = f"{OUTPUT_DIR}/{test_name}_OUT.txt"
 
-                out.write(f"if diff -q <({output}) {solution_file}> /dev/null; then\n")
-                out.write(f"    echo \"Test {plan_file} Passed!\"\n")
+                # the command to be ran
+                command = f"java -jar {JAR} -p {DOMAINS_DIR}/{row['problem']} -pl {plan_file} -g {row['goal']} -vl {NODE_LIMIT} -atl {row['atl']} "
+                command += f"-ctl {row['ctl']} -el {row['el']} -m {search_to_m[row['search']]} -c {cost_to_c[row['cost']]} -h {row['heuristic']} -o {out_file}"
+                print(command)
+
+                # store output in a variable, the solution will get written to a file
+                out.write(f"output=$({command})\n")
+                # get the last line of the output
+                out.write(f"last_line=$(printf '%s\n' \"$output\" | tail -n 1)\n")
+                # if it is a valid solution, the test passes
+                out.write(f"if [[ \"$last_line\" == \"{SUCCESS_MESSAGE}\" ]]; then\n")
+                out.write(f"    echo \"Test {test_name} explained!\"\n")
+                # if it is not a valid solution, the test fails
+                out.write(f"elif [[ \"$last_line\" == \"{FAIL_MESSAGE}\" ]]; then\n")
+                out.write(f"    echo \"! - Test {test_name} not explained within these constraints!\"\n")
+                out.write(f"    ((failures++))\n")
                 out.write(f"else\n")
-                out.write(f"    echo \"Test {plan_file} Failed!\"\n")
+                out.write(f"    echo \"FAIL: unexpected last line: $last_line\"\n")
                 out.write(f"fi\n\n")
 
-#if diff -q <(./my_program input1.txt) solution1.txt > /dev/null; then
-#    echo "Test 1: Passed!"
-#else
-#    echo "Test 1: Failed!"
-#fi
+    # at the end, the program will report how many tests were not explained
+    out.write(f"echo \"Tests not explained: $failures\"")
